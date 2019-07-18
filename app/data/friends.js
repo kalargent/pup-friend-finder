75955dc1ee581f07794d18505cd62d7d2206ef17
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-07-18 14:02:42 -0400

    initial friends.js populated

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+// DEFINE THE INITIAL ARRAY FOR FRIENDS 
+
+var friends = [
+    {
+        name: "Marnie the Dog", 
+        photo: "https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/f_auto,q_auto,w_1100/v1555380344/shape/mentalfloss/primary_glasses.png", 
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    }, 
+
+    {
+        name: "Doug the Pug", 
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMkFm_Se1_oTTZiVkePoSb14RKhKFJu5YbB8roJ4mVXF6aLIqyg", 
+        scores: [5, 1, 2, 2, 3, 1, 4, 5, 5, 5]
+    }, 
+
+    {
+        name: "Handsome Doge", 
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQgv7IoAm9JdEEBMLNQrrmu-GkwC0G2gR2MiTADSS3sHH4-Pl-", 
+        scores: [5, 1, 3, 2, 3, 1, 3, 5, 5, 1]
+    }, 
+
+    {
+        name: "Bart Danzig", 
+        photo: "https://scontent-frx5-1.cdninstagram.com/vp/aae3d6bee26c6cbcc0e189c317a4358a/5D83CDBE/t51.2885-15/e35/46051987_517961745375000_5961132532616550264_n.jpg?_nc_ht=scontent-frx5-1.cdninstagram.com&se=7&ig_cache_key=MTkyNjI5NTI2NDU4MzcyOTA5NQ%3D%3D.2", 
+        scores: [2, 1, 2, 2, 3, 2, 3, 4, 4, 1]
+    }, 
+    {
+        name: "Norbert the Dog", 
+        photo: "https://images.squarespace-cdn.com/content/v1/55a2bffae4b037baec79f96e/1522585429159-Y9PILDV0QNVLP94B5ZMW/ke17ZwdGBToddI8pDm48kGDpvalPb1SqHoCn1hwN0Y57gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmQPoRzxSr1hzN-vPBHt7YyLLXgctAyUJRqJUUGWVDK_ZzIgvsybGcZEPqUYiXY8im/Norbert+colorful+tie.jpg?format=500w", 
+        scores: [4, 4, 2, 1, 3, 5, 3, 2, 4, 1]
+    }, 
+
+
+]
